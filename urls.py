515014652aa6190dc591c268d6436f94fc1f5168
archit nidhi <118736421+architnidhi@users.noreply.github.com>
2025-08-12 20:09:01 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,6 +1,5 @@
-from django.contrib import admin
-from django.urls import path, include
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/todos/', include('todos.urls')),
-]
+from rest_framework.routers import DefaultRouter
+from .views import TodoViewSet
+router = DefaultRouter()
+router.register(r'', TodoViewSet)
+urlpatterns = router.urls
